55195190f044f87dccc2484ed6bbaf0fd731c4e7
RusuGabriel <rusugabriel13@gmail.com>
2020-06-13 16:09:08 +0300

    added tags to some of the tests

--- tests/test_plus.cpp
+++ tests/test_plus.cpp
@@ -1,21 +1,21 @@
 #include "catch.hpp"
 #include "../src/complex.hpp"
 
-TEST_CASE("Test sum: (0,0) + (1,2) ")
+TEST_CASE("Test sum: (0,0) + (1,2) ","[plus]")
 {
     Complex z1, z2(1, 2);
     Complex result(1, 1);
     REQUIRE(z1 + z2 == result);
 }
 
-TEST_CASE("Test sum: (0,0) + (0,0) ")
+TEST_CASE("Test sum: (0,0) + (0,0) ","[plus]")
 {
     Complex z1, z2;
     Complex result(0);
     REQUIRE(z1 + z2 == result);
 }
 
-TEST_CASE("Test sum: (1,3) + (1,2) ")
+TEST_CASE("Test sum: (1,3) + (1,2) ","[plus]")
 {
     Complex z1(1, 3), z2(1, 2);
     REQUIRE(z1 + z2 != 0);
